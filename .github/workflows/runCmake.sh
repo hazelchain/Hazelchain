540d8b7935dc9a3a19d1f689c8ef277ba8d8ed3c
Christian <60714838+Shadow2ube@users.noreply.github.com>
2021-08-04 15:50:31 -0400

    Delete runCmake.sh

--- .github/workflows/runCmake.sh
+++
@@ -1,6 +0,0 @@
-if [${{ matrix.os }} == "winows-latest"]; then
-  cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}} -G "MinGW Makefiles";
-  fi
-if [${{ matrix.os }} == "windows-latest"]; then
-  cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}};
-  fi
