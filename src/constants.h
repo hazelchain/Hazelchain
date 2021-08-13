48f3cfd01c4af604fcc15e2a606e0e2beff12067
Christian <christianlaforest45@gmail.com>
2021-08-13 00:30:40 -0400

    Constants and stuff - a temporary solution for the logging issues, might fix later

---
+++ src/constants.h
@@ -0,0 +1,20 @@
+//
+// Created by chris on 2021-08-12.
+//
+
+#ifndef HAZELCHAIN_CONSTANTS_H
+#define HAZELCHAIN_CONSTANTS_H
+
+namespace constants {
+    Logger logger(
+            util::concat(
+                    util::currentTime("[%d-%m-%y  %H:%M:%S] "),
+                    "-log.txt"
+            ),
+            std::cout
+    );
+
+    json settings = {};
+}
+
+#endif //HAZELCHAIN_CONSTANTS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,78 @@
-
-#include <sys/stat.h>
 #include <json.hpp>
-#include "Block.h"
 #include "storage/logging/Logger.h"
+#include "constants.h"
+#include "node/client.h"
 
 #ifdef _WIN32
 #define mkdir(a, b) mkdir(a) // discard 2nd argument on windows
 #endif // _WIN32
 
-using namespace std;
 using json = nlohmann::json;
 
-void sync(Logger &logger);
+void loadSettings();
 
-void setupDirectories(Logger &logger);
+void sync();
 
-void generateDir(const char *name, Logger &logger);
+void setupDirectories();
+
+void generateDir(const char *name);
 
 int findNodes();
 
-void loadSettings(Logger &logger);
-
-vector<Block> chain;
-json settings = {};
 
 int main(int argc, char **argv) {
-    Logger logger(
-            util::concat(
-                    util::currentTime("[%d-%m-%y  %H:%M:%S] "),
-                    "-log.txt"
-            ),
-            std::cout
-    );
-
-    loadSettings(logger);
-    if (!util::contains(argc, argv, "-nosync")) sync(logger);
+    loadSettings();
+    if (!util::contains(argc, argv, "-nosync")) sync();
 
     return 0;
 }
 
-void sync(Logger &logger) {
-    log(logger, info)
+void loadSettings() {
+    log(constants::logger, info) << "Checking for existing settings" << '\n';
+
+    json target = {
+            {"ip",           util::getIp()},
+            {"server_port",  10541},
+            {"test_port",    10542},
+            {"api_port",     10543},
+            {"genesis_hash", util::generateGenesisHash()},
+            {"nodes",        {
+                                     {"fallback", {"-1"}},
+                                     {"found", nullptr}
+                             }}
+    };
+
+    json recovered = util::loadJson("settings.json", target);
+    if (recovered["nodes"]["fallback"] != target["nodes"]["fallback"])
+        recovered["nodes"]["fallback"] = target["nodes"]["fallback"];
+    constants::settings = recovered;
+
+    ofstream o("settings.json");
+    o << constants::settings;
+    o.close();
+}
+
+void sync() {
+    log(constants::logger, info)
             << "Generating directories if they don't exist"
             << '\n';
 
-    setupDirectories(logger);
+    setupDirectories();
 
-    log(logger, info)
+    log(constants::logger, info)
             << "Indexing and checking blocks"
             << '\n';
 
-    log(logger, info)
+    log(constants::logger, info)
             << "Genesis block _hash: "
-            << settings["genesis_hash"]
+            << constants::settings["genesis_hash"]
             << '\n';
 
-    log(logger, info)
+    log(constants::logger, info)
             << "syncing to other nodes"
             << '\n';
 
-    log(logger, info)
+    log(constants::logger, info)
             << "connected to "
             << util::to_string<int>(findNodes())
             << " other nodes"
@@ -73,44 +86,23 @@ int findNodes() {
     return -1;
 }
 
-void setupDirectories(Logger &logger) {
-    generateDir("blocks", logger);
-    generateDir("trees", logger);
+void setupDirectories() {
+    generateDir("blocks");
+    generateDir("trees");
 }
 
-void generateDir(const char *name, Logger &logger) {
+void generateDir(const char *name) {
     if (!util::exists(name)) {
         if (mkdir(name, 0) != 0) {
-            log(logger, info)
+            log(constants::logger, info)
                     << "Could not create folder \""
                     << name
                     << "\"";
         }
     } else {
-        log(logger, info)
+        log(constants::logger, info)
                 << "Directory \""
                 << name
                 << "\" already exists";
     }
-}
-
-void loadSettings(Logger &logger) {
-    log(logger, info) << "Checking for existing settings" << '\n';
-
-    json recovered = util::loadJson("settings.json", {
-            {"ip",           util::getIp()},
-            {"server_port",  10541},
-            {"test_port",    10542},
-            {"api_port",     10543},
-            {"genesis_hash", util::generateGenesisHash()},
-            {"nodes",        {
-                                     {"fallback", {"-1"}},
-                                     {"found", nullptr}
-                             }}
-    });
-    settings = recovered;
-
-    ofstream o("settings.json");
-    o << settings;
-    o.close();
 }
