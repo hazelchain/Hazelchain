e7e9f24ac6844cd2cd9c4621ec09e622b4f09d93
Christian <christianlaforest45@gmail.com>
2021-08-13 22:42:23 -0400

    a few bug fixes and stuff

--- windows.bat
+++ windows.bat
@@ -1,8 +1,6 @@
-@REM @echo off
+@echo off
 
-echo "Directory: %~f0"
-
-cd C:/
+cd \
 if not exist dev\ ( mkdir dev )
 cd dev
 if not exist vcpkg\vcpkg.exe ( git clone https://github.com/Microsoft/vcpkg.git )
@@ -13,9 +11,7 @@ if /I "%PARENT%" == "powershell" goto :POWERSHELL
 if /I "%PARENT%" == "pwsh" goto :POWERSHELL
 
 call bootstrap-vcpkg.bat -disableMetrics
-call vcpkg.exe install --recurse ^
-openssl:x64-windows ^
-sqlite3:x64-windows ^
+call vcpkg.exe install --recurse openssl:x64-windows sqlite3:x64-windows
 
 call vcpkg.exe integrate install
 echo done!
@@ -23,7 +19,7 @@ goto :EOF
 
 :POWERSHELL
 call .\bootstrap-vcpkg.bat -disableMetrics
-call .\vcpkg.exe install openssl:x64-windows sqlite3:x64-windows
+call .\vcpkg.exe install --recurse openssl:x64-windows sqlite3:x64-windows
 call .\vcpkg.exe integrate install
 echo done!
 goto :EOF
@@ -31,3 +27,7 @@ goto :EOF
 :GETPARENT
 SET "PSCMD=$ppid=$pid;while($i++ -lt 3 -and ($ppid=(Get-CimInstance Win32_Process -Filter ('ProcessID='+$ppid)).ParentProcessId)) {}; (Get-Process -EA Ignore -ID $ppid).Name"
 for /f "tokens=*" %%i in ('powershell -noprofile -command "%PSCMD%"') do SET %1=%%i
+
+@REM k, so in a powershell do the following```
+@REM cd / && ls dev
+@REM ``` and send me the output
