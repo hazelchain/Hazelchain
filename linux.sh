2c6d6fb6fe1c1b4958915dbc056c1a87b3cd9169
Christian <christianlaforest45@gmail.com>
2021-09-10 13:08:19 -0400

    hopefully, linux - linux.sh should work for linux but would anyways fail later

--- linux.sh
+++ linux.sh
@@ -1,13 +1,8 @@
 # WARNING: not currently developing for linux -- this won't work
 
-echo "Directory: $PWD"
-
-cd ~
-if [ ! -d "dev" ]; then mkdir dev; fi
-cd dev
 if [ ! -d "vcpkg" ]; then git clone https://github.com/Microsoft/vcpkg.git; fi
 cd vcpkg
 
 ./bootstrap-vcpkg.sh -disableMetrics
-call .\vcpkg.exe install openssl
+call .\vcpkg.exe install --recurse openssl:x64-windows sqlite3:x64-windows
 ./vcpkg integrate install
