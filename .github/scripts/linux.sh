2c4989ff74c9d40baba6f50ebc52e998c0666c42
Christian <christianlaforest45@gmail.com>
2021-09-10 14:01:28 -0400

    edited cmake.yml

--- .github/scripts/linux.sh
+++ .github/scripts/linux.sh
@@ -1,10 +1,10 @@
 # WARNING: not currently developing for linux -- this won't work
 cd ../../
 
-if [test -f "vcpkg\vcpkg.exe"]; then
+if [ -f "vcpkg\vcpkg.exe"]; then
   git clone https://github.com/Microsoft/vcpkg.git
 fi
 
 sh ./vcpkg/bootstrap-vcpkg.sh -disableMetrics
-sh ./vcpkg/vcpkg install --recurse openssl:x64-windows sqlite3:x64-windows
-sh ./vcpkg/ vcpkg integrate install
+sh ./vcpkg/vcpkg.exe install --recurse openssl:x64-windows sqlite3:x64-windows
+sh ./vcpkg/vcpkg.exe integrate install
